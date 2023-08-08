c0952feef2f03e77d0b8dd1ef83eb607473b8bdf
Rayato159 <rayato159@gmail.com>
2023-08-09 00:05:41 +0700

    -feature :sparkles: add abstract factory method

--- creation/abstractFactory/main.go
+++ creation/abstractFactory/main.go
@@ -1,5 +1,64 @@
 package main
 
-func main() {
+import "fmt"
 
+type Mage interface {
+	GetWeapon() string
+	GetSkill() string
+}
+
+type mage struct {
+	weapon string
+	skill  string
+}
+
+type MageFactory struct {
+	Factory Mage
+}
+
+func NewMageFactory(factory Mage) *MageFactory {
+	return &MageFactory{
+		Factory: factory,
+	}
+}
+
+type pyromancer struct {
+	*mage
+}
+
+func NewPyromancer() Mage {
+	return &pyromancer{
+		&mage{
+			weapon: "Fist",
+			skill:  "Fire ball",
+		},
+	}
+}
+
+type preist struct {
+	*mage
+}
+
+func NewPreist() Mage {
+	return &preist{
+		&mage{
+			weapon: "Staff of healing",
+			skill:  "Healing",
+		},
+	}
+}
+
+func (m *mage) GetWeapon() string { return m.weapon }
+func (m *mage) GetSkill() string  { return m.skill }
+
+func main() {
+	mage := NewMageFactory(NewPyromancer())
+	fmt.Println(mage.Factory.GetWeapon())
+	fmt.Println(mage.Factory.GetSkill())
+
+	fmt.Println()
+
+	mage.Factory = NewPreist()
+	fmt.Println(mage.Factory.GetWeapon())
+	fmt.Println(mage.Factory.GetSkill())
 }
